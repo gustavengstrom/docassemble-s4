fff1141ad26768f9c8b5640532726d11be35f22e
Gustav E <gustav@davcon.se>
2024-11-25 17:43:27 +0100

    new

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.s4',
-      version='0.0.5',
+      version='0.0.6',
       description=('A docassemble extension.'),
       long_description='# docassemble.s4\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nGustav E, gustav@davcon.se\r\n\r\n',
       long_description_content_type='text/markdown',
